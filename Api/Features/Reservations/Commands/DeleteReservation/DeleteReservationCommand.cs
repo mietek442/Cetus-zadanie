ae357b0f5a26f599c569fb7f3f123b0df3a7c1be
mietek442 <96135826+mietek442@users.noreply.github.com>
2026-07-27 19:52:43 +0200

    Rezewrwacje dodanie zmiany statusu, dodanie usuwania rezerwacji( na razie na szytywno ale do zmieniania na isDelate)  wyswietlanie wszystkich rezerwacji

---
+++ Api/Features/Reservations/Commands/DeleteReservation/DeleteReservationCommand.cs
@@ -0,0 +1,41 @@
+﻿namespace Api.Features.Reservations.Commands.DeleteReservation
+{
+    using Api.Infrastructure.DbContext;
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class DeleteReservationCommand : IRequest<ActionResult<Guid>>
+    {
+        public Guid ReservationId;
+    }
+
+    public class DeleteReservationCommandHandler
+        : IRequestHandler<DeleteReservationCommand, ActionResult<Guid>>
+    {
+        private readonly IApplicationContext _context;
+
+        public DeleteReservationCommandHandler(IApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<Guid>> Handle(
+            DeleteReservationCommand request,
+            CancellationToken cancellationToken)
+        {
+            var reservation = await _context.Reservations
+                .FindAsync(request.ReservationId, cancellationToken);
+
+            if (reservation == null)
+            {
+                return new NotFoundResult();
+            }
+
+            _context.Reservations.Remove(reservation);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new OkObjectResult(request.ReservationId);
+        }
+    }
+}
---
+++ Api/Features/Reservations/Queries/GetAllReservations/GetAllReservationsQuery.cs
@@ -0,0 +1,41 @@
+﻿using Api.Domain.Models;
+using Api.Infrastructure.DbContext;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Features.Reservations.Queries.GetAllReservations
+{
+    public class GetAllReservationsQuery : IRequest<ActionResult<List<Reservation>>>
+    {
+    }
+
+    public class GetAllReservationsQueryHandler
+        : IRequestHandler<GetAllReservationsQuery, ActionResult<List<Reservation>>>
+    {
+        private readonly IApplicationContext _context;
+
+        public GetAllReservationsQueryHandler(IApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<List<Reservation>>> Handle(
+            GetAllReservationsQuery request,
+            CancellationToken cancellationToken)
+        {
+            var reservations = await _context.Reservations
+                .Include(reservation => reservation.Desk)
+                .ToListAsync(cancellationToken);
+
+
+            if (reservations == null || reservations.Count == 0)
+            {
+                return new NotFoundResult();
+            }
+
+
+            return new OkObjectResult(reservations);
+        }
+    }
+}
